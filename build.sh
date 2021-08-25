4d734e1956f83ce223104cf71f48c23f3e5e8207
lebhoryi <lebhoryi@rt-thread.com>
2021-08-25 19:08:51 +0800

    create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -d "$CI_PROJECT_DIR/课后拓展" ]; then 
+    rm -rf K210_BSP/applications/* 
+    cp $CI_PROJECT_DIR/课后拓展/代码/* K210_BSP/applications 
+    scons -C K210_BSP -j$CPUS
+    echo "Done."
+fi
+
