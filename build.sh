a53ba81f370a85afe51119c1b16103b4f2a22712
lebhoryi <lebhoryi@rt-thread.com>
2021-08-26 10:02:19 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -d "$CI_PROJECT_DIR/课后拓展" ]; then 
     rm -rf K210_BSP/applications/* 
-    cp $CI_PROJECT_DIR/课后拓展/代码/* K210_BSP/applications 
+    cp $CI_PROJECT_DIR/课后拓展/代码/* K210_BSP/applications -r
     scons -C K210_BSP -j$CPUS
     echo "Done."
 fi
