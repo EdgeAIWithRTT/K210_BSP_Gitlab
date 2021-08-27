62d5a186306bbc2af926c00e73b1b7ec36e9a95a
lebhoryi <lebhoryi@rt-thread.com>
2021-08-27 15:38:48 +0800

    add copy bin file

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 if [ -d "$CI_PROJECT_DIR/课后拓展" ]; then 
     rm -rf K210_BSP/applications/* 
     cp $CI_PROJECT_DIR/课后拓展/代码/* K210_BSP/applications -r
-    scons -c
+    scons -C K210_BSP -c
     scons -C K210_BSP -j$CPUS
+    cp K210_BSP/rtthread.bin $CI_PROJECT_DIR/课后拓展/固件
     echo "Done."
 fi
 
